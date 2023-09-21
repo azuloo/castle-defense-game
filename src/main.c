6341876e0e9ce7c65f697861c97484b475156f32
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2023-09-21 14:42:57 +0300

    Update main.c
    Add graphics lib test in main (WIP)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,39 @@
+#include "graphics.h"
 
-int main(int argc, int *argv[])
+int main(int argc, int* argv[])
 {
+	static float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	static float vertices2[] = {
+		-0.5f, -0.25f, 0.0f,
+		0.5f, -0.25f, 0.0f,
+		0.0f, 0.75f, 0.0f
+	};
+
+	static float vertices3[] = {
+		-0.5f, -0.75f, 0.0f,
+		0.5f, -0.75f, 0.0f,
+		0.0f, 0.25f, 0.0f
+	};
+
+	init_graphics();
+
+	BackgroundColor b_Color;
+	b_Color.R = 0.3f;
+	b_Color.G = 0.4f;
+	b_Color.B = 0.5f;
+	b_Color.A = 1.0f;
+	set_background_color(b_Color);
+
+	draw_triangle(vertices, 9);
+	draw_triangle(vertices2, 9);
+	draw_triangle(vertices3, 9);
+	
+	draw();
+
 	return 0;
 }
