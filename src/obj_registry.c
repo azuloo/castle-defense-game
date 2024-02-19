fd399b4c7456df1e02e62a09380b585fdcfed910
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-02-19 12:35:56 +0200

    Added obj_registry memory realloc

--- src/obj_registry.c
+++ src/obj_registry.c
@@ -1,58 +1,69 @@
 #include "obj_registry.h"
 
-static void** g_Registry = NULL;
-static int    g_RegistryEntires = 32;
-static int    g_Count = 0;
+static void**   s_Registry = NULL;
+static int      s_RegistryCapacity = 32;
+static int      s_RegistryObjCount = 0;
+
+// ! Allocates memory on heap !
+static int alloc_registry()
+{
+	s_RegistryCapacity *= 2;
+	void** reg = realloc(s_Registry, s_RegistryCapacity * sizeof *reg);
+	if (NULL == reg)
+	{
+		PRINT_ERR("[obj_registry]: Failed to allocate sufficient memory for Registry ptr.");
+		return TERMINATE_ERR_CODE;
+	}
+
+	s_Registry = reg;
+	
+	return 0;
+}
+
+// ----------------------- PUBLIC FUNCTIONS ----------------------- //
 
 void registry_free()
 {
-	if (NULL == g_Registry)
+	if (NULL == s_Registry)
 	{
 		return;
 	}
 
-	free(g_Registry);
+	free(s_Registry);
 }
 
 int register_obj(void* obj, int* handle)
 {
-	if (g_Count == g_RegistryEntires)
+	if (NULL == s_Registry || s_RegistryObjCount == s_RegistryCapacity)
 	{
-		// TODO: Allocate more memory
-	}
-
-	if (NULL == g_Registry)
-	{
-		// TODO: Free memory
-		void** reg = malloc(g_RegistryEntires * sizeof *reg);
-		if (NULL == reg)
+		int alloc_registry_res = alloc_registry();
+		if (TERMINATE_ERR_CODE == alloc_registry_res)
 		{
-			PRINT_ERR("[obj_registry]: Failed to allocate sufficient memory for Registry ptr.");
-			return 0;
+			return TERMINATE_ERR_CODE;
 		}
-
-		g_Registry = reg;
 	}
 
-	g_Registry[g_Count] = obj;
-	*handle = g_Count;
-	g_Count++;
+	s_Registry[s_RegistryObjCount] = obj;
+	*handle = s_RegistryObjCount;
+	s_RegistryObjCount++;
 
 	return 0;
 }
 
 void* get_obj_from_registry(const int* handle)
 {
-	if (NULL == g_Registry)
+	if (NULL == s_Registry)
 	{
 		PRINT_ERR("[obj_registry]: Registry was not initialized.");
 		return NULL;
 	}
-	if (*handle >= g_Count)
+	if (*handle >= s_RegistryObjCount)
 	{
 		PRINT_ERR("[obj_registry]: Handle value is greater than registry entries count.");
 		return NULL;
 	}
 
-	return g_Registry[*handle];
+	return s_Registry[*handle];
 }
+
+// ----------------------- PUBLIC FUNCTIONS END ----------------------- //
