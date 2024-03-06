ed62897fd6b14d050ca1c164b36fac3822582c12
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-03-06 17:21:10 +0200

    Fixed error func reporting critical error

--- include/utils.h
+++ include/utils.h
@@ -21,7 +21,7 @@ static char err_msg_buffer[1024];
 #define PRINT_ERR(msg) fprintf(stderr, "ERR: %s:%d: %s\n", __FILE__, __LINE__, msg)
 // TODO: Is there a better way to do this?
 #define PRINT_ERR_VARGS(msg, ...) \
-	sprintf_s(err_msg_buffer, msg, __VA_ARGS__); \
+	sprintf(err_msg_buffer, msg, __VA_ARGS__); \
 	PRINT_ERR(err_msg_buffer); \
 	memset(err_msg_buffer, 0, sizeof(err_msg_buffer));
 
