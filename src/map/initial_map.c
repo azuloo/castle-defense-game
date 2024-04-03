1973af36e3adb2f25995140427ed64040a057167
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-04-03 17:17:52 +0300

    Replaced manual drawable creation for draw_quad()

--- src/map/initial_map.c
+++ src/map/initial_map.c
@@ -20,46 +20,21 @@ static PathSegment** s_Path = NULL;
 
 int initial_add_background()
 {
-	// TODO: Replace with quad_draw()
-	DrawBufferData* draw_buf_data = NULL;
-	get_quad_draw_buffer_data(&draw_buf_data);
-	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != draw_buf_data, "[initial_map]: Failed to retrieve square DrawBufferData.");
-
-	static const char* vertex_shader_path = "/res/static/shaders/basic_vert.txt";
-	static const char* fragment_shader_path = "/res/static/shaders/basic_frag.txt";
 	static const char* texture_path = "/res/static/textures/field.jpg";
 
-	DrawableDef* drawable = NULL;
-	create_drawable(&drawable);
-	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != drawable, "[initial_map]: Drawable is not defined.");
-
-	add_texture_2D(drawable, texture_path, TexType_RGB);
-
-	int add_res = setup_drawable(drawable, draw_buf_data, vertex_shader_path, fragment_shader_path);
-	CHECK_EXPR_FAIL_RET_TERMINATE(TERMINATE_ERR_CODE != add_res, "[initial_map]: Failed to add env element.");
-
-	register_drawable_attribute(drawable, POS_TEXTURE_ATTRIBUTE_SIZE); // Position + texutre.
-	process_drawable_attributes(drawable);
-
 	Vec3 translation = { { wWidth / 2.f, wHeight / 2.f, Z_DEPTH_INITIAL_MAP_BACKGROUND } };
-	drawable->transform.translation = translation;
-
 	Vec3 scale = { { wWidth / 2.f, wHeight / 2.f, 1.f } };
-	drawable->transform.scale = scale;
+	Vec4 color = { { 1.f, 1.f, 1.f, 1.f } };
 
-	drawable_transform_ts(drawable, COMMON_MODEL_UNIFORM_NAME);
-
-	drawable->matrices.projection = COMMON_ORTHO_MAT;
-	add_uniform_mat4f(drawable->shader_prog, COMMON_PROJECTION_UNIFORM_NAME, &drawable->matrices.projection);
+	DrawableDef* drawable = NULL;
+	draw_quad(&drawable, texture_path, TexType_RGB, &translation, &scale, &color);
+	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != drawable, "[entity]: Failed to draw triangle entity (empty quad drawable).");
 
 	return 0;
 }
 
 int initial_add_path()
 {
-	// TODO: Replace with quad_draw()?
-	static const char* vertex_shader_path = "/res/static/shaders/basic_vert.txt";
-	static const char* fragment_shader_path = "/res/static/shaders/basic_frag.txt";
 	static const char* texture_path = "/res/static/textures/road.jpg";
 
 	// TODO: Clear up "magic"
@@ -90,24 +65,10 @@ int initial_add_path()
 
 	s_Path = path;
 
+	Vec4 color = { { 1.f, 1.f, 1.f, 1.f } };
+
 	for (int i = 0; i < INTIAL_MAP_PATH_LEN; i++)
 	{
-		DrawBufferData* draw_buf_data = NULL;
-		get_quad_draw_buffer_data(&draw_buf_data);
-		CHECK_EXPR_FAIL_RET_TERMINATE(NULL != draw_buf_data, "[initial_map]: Failed to retrieve square DrawBufferData.");
-
-		DrawableDef* drawable = NULL;
-		create_drawable(&drawable);
-		CHECK_EXPR_FAIL_RET_TERMINATE(NULL != drawable, "[initial_map]: Drawable is not defined.");
-
-		add_texture_2D(drawable, texture_path, TexType_RGB);
-
-		int add_res = setup_drawable(drawable, draw_buf_data, vertex_shader_path, fragment_shader_path);
-		CHECK_EXPR_FAIL_RET_TERMINATE(TERMINATE_ERR_CODE != add_res, "[initial_map]: Failed to add initial map level\'s element.");
-
-		register_drawable_attribute(drawable, POS_TEXTURE_ATTRIBUTE_SIZE); // Position + texture.
-		process_drawable_attributes(drawable);
-
 		PathSegment* path_segment = s_Path[i];
 
 		float pos_x_diff = path_segment->end.x - path_segment->start.x;
@@ -147,15 +108,11 @@ int initial_add_path()
 		float pos_y = (path_segment->start.y + path_segment->end.y) / 2.f;
 
 		Vec3 translation = { { pos_x, pos_y, Z_DEPTH_INITIAL_MAP_PATH } };
-		drawable->transform.translation = translation;
-
 		Vec3 scale = { { scale_x, scale_y, 1.f } };
-		drawable->transform.scale = scale;
 
-		drawable_transform_ts(drawable, COMMON_MODEL_UNIFORM_NAME);
-
-		drawable->matrices.projection = COMMON_ORTHO_MAT;
-		add_uniform_mat4f(drawable->shader_prog, "projection", &drawable->matrices.projection);
+		DrawableDef* drawable = NULL;
+		draw_quad(&drawable, texture_path, TexType_RGB, &translation, &scale, &color);
+		CHECK_EXPR_FAIL_RET_TERMINATE(NULL != drawable, "[entity]: Failed to draw triangle entity (empty quad drawable).");
 	}
 
 	return 0;
