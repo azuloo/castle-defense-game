d2bbcb2238b5de5f0db796094e90c2f92badf8bf
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-04-20 16:18:59 +0300

    Fixed error reporting macros

--- include/utils.h
+++ include/utils.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <assert.h>
 #include <stdbool.h>
@@ -17,31 +18,29 @@
 
 #define APP_EXIT(code) exit(code)
 
-static char err_msg_buffer[1024];
+#define ERR_MSG_BUFFER_LEN 1024
+static char err_msg_buffer[ERR_MSG_BUFFER_LEN];
 
-#define PRINT_ERR(msg) fprintf(stderr, "ERR: %s:%d: %s\n", __FILE__, __LINE__, msg)
-// TODO: Is there a better way to do this?
-#define PRINT_ERR_VARGS(msg, ...) \
-	sprintf(err_msg_buffer, msg, __VA_ARGS__); \
-	PRINT_ERR(err_msg_buffer); \
-	memset(err_msg_buffer, 0, sizeof(err_msg_buffer));
+void print_err(const char* format, ...);
+void str_concat(const char* s1, const char* s2, char** buf, size_t len);
 
-#define CHECK_EXPR_FAIL_RET_TERMINATE(expr, msg) \
+#define PRINT_ERR(format, ...) fprintf(stderr, "ERR: %s:%d: %s\n", __FILE__, __LINE__, format)
+#define PRINT_ERR_VARGS(format, ...) print_err(format, __VA_ARGS__)
+
+#define CHECK_EXPR_FAIL_RET_TERMINATE(expr, format, ...) \
 	if (!(expr)) { \
-		PRINT_ERR(msg); \
+		PRINT_ERR_VARGS(format, __VA_ARGS__); \
 		return TERMINATE_ERR_CODE; \
 	}
-#define CHECK_EXPR_FAIL_RET_NULL(expr, msg) \
+#define CHECK_EXPR_FAIL_RET_NULL(expr, format, ...) \
 	if (!(expr)) { \
-		PRINT_ERR(msg); \
+		PRINT_ERR_VARGS(format, __VA_ARGS__); \
 		return NULL; \
 	}
-#define CHECK_EXPR_FAIL_RET(expr, msg) \
+#define CHECK_EXPR_FAIL_RET(expr, format, ...) \
 	if (!(expr)) { \
-		PRINT_ERR(msg); \
+		PRINT_ERR_VARGS(format, __VA_ARGS__); \
 		return; \
 	}
-
-void str_concat(const char* s1, const char* s2, char** buf, size_t len);
 
 #endif // _UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -7,3 +7,13 @@ void str_concat(const char* s1, const char* s2, char** buf, size_t len)
 {
 	snprintf(buf, sizeof(buf) * len, "%s%s", s1, s2);
 }
+
+void print_err(char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vsnprintf(err_msg_buffer, ERR_MSG_BUFFER_LEN, format, args);
+	PRINT_ERR(err_msg_buffer);
+	memset(err_msg_buffer, 0, sizeof(err_msg_buffer));
+	va_end(args);
+}
