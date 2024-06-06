1f5170ada60f44c42feaa9390c137e8d86b89697
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-06-06 23:46:17 +0300

    Added health bar def

--- include/health_bar.h
+++ include/health_bar.h
@@ -4,6 +4,17 @@
 #include "global_decl.h"
 #include "lin_alg.h"
 
+#define HEALTH_BAR_DEFAULT_VALUE 100
+
+typedef struct HealthBarDef
+{
+	Collidable2D* collision_box;
+	int value;
+	int handle;
+	int drawable_handle;
+} HealthBarDef;
+
 int add_health_bar(const Vec3* pos, const Vec3* scale);
+void health_bar_free_resources();
 
 #endif // _HEALTH_BAR_H
--- src/health_bar.c
+++ src/health_bar.c
@@ -1,5 +1,6 @@
 #include "health_bar.h"
 #include "graphics.h"
+#include "physics.h"
 #include "drawable_ops.h"
 #include "global_defs.h"
 #include "utils.h"
@@ -7,6 +8,40 @@
 static const char* s_HealthBarOuterTexture = "/res/static/textures/health_bar.png";
 static const char* s_quad_vertex_shader_path = "/res/static/shaders/basic_vert.txt";
 static const char* s_quad_frag_shader_path = "/res/static/shaders/health_bar_frag.txt";
+
+static HealthBarDef* s_HealthBar = NULL;
+static int s_HealthBarCapacity = 1;
+static int s_HealthBarCount = 0;
+
+static int alloc_heath_bar_arr()
+{
+	s_HealthBarCapacity *= 2;
+	HealthBarDef* health_bar = realloc(s_HealthBar, s_HealthBarCapacity * sizeof *health_bar);
+	CHECK_EXPR_FAIL_RET_TERMINATE(health_bar != NULL, "[health_bar]: Failed to allocate enough memory for health bar arr.");
+
+	s_HealthBar = health_bar;
+
+	return 0;
+}
+
+static int create_health_bar(HealthBarDef** dest)
+{
+	if (NULL == s_HealthBar || s_HealthBarCount >= s_HealthBarCapacity)
+	{
+		int alloc_health_bar_arr_res = alloc_heath_bar_arr();
+		CHECK_EXPR_FAIL_RET_TERMINATE(TERMINATE_ERR_CODE != alloc_health_bar_arr_res, "[health_bar]: Failed to create health bar arr.");
+	}
+
+	HealthBarDef* health_bar = s_HealthBar + s_HealthBarCount;
+	health_bar->collision_box = NULL;
+	health_bar->value = HEALTH_BAR_DEFAULT_VALUE;
+	health_bar->drawable_handle = -1;
+
+	s_HealthBarCount++;
+	*dest = health_bar;
+
+	return 0;
+}
 
 int add_health_bar(const Vec3* pos, const Vec3* scale)
 {
@@ -25,7 +60,8 @@ int add_health_bar(const Vec3* pos, const Vec3* scale)
 	int add_res = setup_drawable(drawable, draw_buf_data, s_quad_vertex_shader_path, s_quad_frag_shader_path);
 	CHECK_EXPR_FAIL_RET_TERMINATE(TERMINATE_ERR_CODE != add_res, "[health_bar]: Failed to add env element.");
 
-	register_drawable_attribute(drawable, POS_TEXTURE_ATTRIBUTE_SIZE);       // Position + texture.
+	// TODO: Replace with common method (e.g. draw_quad)
+	register_drawable_attribute(drawable, POS_TEXTURE_ATTRIBUTE_SIZE); // Position + texture.
 	process_drawable_attributes(drawable);
 
 	drawable->transform.translation = *pos;
@@ -42,5 +78,19 @@ int add_health_bar(const Vec3* pos, const Vec3* scale)
 	add_uniform_vec4f(drawable->shader_prog, COMMON_COLOR_UNIFORM_NAME, &color);
 	add_uniform_1f(drawable->shader_prog, "uFilled", 1.f);
 
+	HealthBarDef* health_bar = NULL;
+	create_health_bar(&health_bar);
+	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != health_bar, "[health_bar]: Failed to create a health bar.");
+
+	health_bar->drawable_handle = drawable->handle;
+
 	return 0;
+}
+
+void health_bar_free_resources()
+{
+	if (NULL != s_HealthBar)
+	{
+		free(s_HealthBar);
+	}
 }
