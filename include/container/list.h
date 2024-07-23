55aff77037b740d1c5309eadc84737c273f6b839
Taras Gnatiuk <gnatiyk.taras@gmail.com>
2024-07-23 13:13:19 +0300

    Added list impl

---
+++ include/container/list.h
@@ -0,0 +1,24 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+typedef struct ListNode ListNode;
+
+typedef struct ListNode
+{
+	ListNode* prev;
+	ListNode* next;
+	void* data;
+} ListNode;
+
+typedef struct List
+{
+	ListNode* head;
+	ListNode* tail;
+	int length;
+} List;
+
+int create_list(List** dest);
+int add_element_to_list(List* list, void* data, int data_len);
+int free_list(List* list);
+
+#endif // _LIST_H
---
+++ src/container/list.c
@@ -0,0 +1,51 @@
+#include "container/list.h"
+#include "utils.h"
+
+int create_list(List** dest)
+{
+	List* list = malloc(sizeof *list);
+	CHECK_EXPR_FAIL_RET_TERMINATE(list != NULL, "[list]: Failied to allocate sufficient memory for the list.");
+
+	list->head = NULL;
+	list->tail = NULL;
+	list->length = 0;
+
+	*dest = list;
+
+	return 0;
+}
+
+int add_element_to_list(List* list, void* data, int data_len)
+{
+	ListNode* new_node = malloc(sizeof *new_node);
+	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != new_node, "[list]: Failed to allocate sufficient memory for the list.");
+	void* data_ptr = malloc(data_len);
+	CHECK_EXPR_FAIL_RET_TERMINATE(NULL != new_node, "[list]: Failed to allocate sufficient memory for the list data.");
+
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->data = data_ptr;
+
+	memcpy(new_node->data, data, data_len);
+
+	if (NULL == list->head)
+	{
+		list->head = new_node;
+		list->tail = new_node;
+	}
+	else
+	{
+		list->tail->next = new_node;
+		list->tail = new_node;
+	}
+
+	list->length++;
+
+	return 0;
+}
+
+// TODO: Implement
+int free_list(List* list)
+{
+	return 0;
+}
